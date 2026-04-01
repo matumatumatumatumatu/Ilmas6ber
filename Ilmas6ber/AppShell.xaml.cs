a8cc70eaf85eda5d210574c62920eda19f0e3d59
opilane <opilane@HP-CZC3217062>
2026-04-01 10:31:13 +0300

    Add project files.

---
+++ Ilmas6ber/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ilmas6ber
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Ilmas6ber/Platforms/Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace Ilmas6ber
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
