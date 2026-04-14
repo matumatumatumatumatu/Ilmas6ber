83d3df8416c5be63e1e59bd584650fb61723da66
Opilane TTHK <144818434+Dualifyy@users.noreply.github.com>
2026-04-14 09:16:10 +0300

    Initialized map part 1
    Not sure if it works right now.

--- Ilmas6ber/MainPage.xaml.cs
+++ Ilmas6ber/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace Ilmas6ber
+﻿using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+
+namespace Ilmas6ber
 {
     public partial class MainPage : ContentPage
     {
@@ -6,7 +9,12 @@
 
         public MainPage()
         {
-            
+            InitializeComponent();
+
+            var location = new Location(59.437, 24.7536); // Tallinn
+            var span = MapSpan.FromCenterAndRadius(location, Distance.FromKilometers(5));
+
+            map.MoveToRegion(span);
         }
     }
 }
