c5636f8d653a5e047ce9d9f04dcd0ab47e23635c
dualify <144818434+Dualifyy@users.noreply.github.com>
2026-05-06 09:15:42 +0300

    Added a working map this time
    -dualify

--- Ilmas6ber/MainPage.xaml.cs
+++ Ilmas6ber/MainPage.xaml.cs
@@ -5,6 +5,10 @@
         public MainPage()
         {
             InitializeComponent();
+
+            var mapControl = new Mapsui.UI.Maui.MapControl();
+            mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+            Content = mapControl;
         }
     }
 }
--- Ilmas6ber/MauiProgram.cs
+++ Ilmas6ber/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace Ilmas6ber
 {
@@ -9,6 +10,7 @@ namespace Ilmas6ber
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseSkiaSharp()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
