607bffb7c9b7166615c76a4dc5d94e402d0f77ad
dualify <144818434+Dualifyy@users.noreply.github.com>
2026-05-06 09:52:45 +0300

    Added zoom button
    -dualify

--- Ilmas6ber/MainPage.xaml.cs
+++ Ilmas6ber/MainPage.xaml.cs
@@ -1,4 +1,13 @@
-﻿namespace Ilmas6ber
+﻿using Mapsui;
+using Mapsui.Extensions;
+using Mapsui.Tiling;
+using Mapsui.Widgets;
+using Mapsui.Widgets.ScaleBar;
+using Mapsui.Widgets.ButtonWidgets;
+using System.Threading.Tasks;
+using Mapsui.Widgets.InfoWidgets;
+
+namespace Ilmas6ber
 {
     public partial class MainPage : ContentPage
     {
@@ -9,6 +18,15 @@
             var mapControl = new Mapsui.UI.Maui.MapControl();
             mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
             Content = mapControl;
+            mapControl.Map?.Layers.Add(OpenStreetMap.CreateTileLayer());
+            mapControl.Map?.Widgets.Add(new ScaleBarWidget(mapControl.Map)
+            {
+                TextAlignment = Alignment.Center,
+                HorizontalAlignment = Mapsui.Widgets.HorizontalAlignment.Center,
+                VerticalAlignment = Mapsui.Widgets.VerticalAlignment.Top
+            });
+            mapControl.Map?.Widgets.Add(new ZoomInOutWidget { Margin = new MRect(20, 40) });
+            mapControl.Map?.Widgets.Add(new MouseCoordinatesWidget());
         }
     }
 }
