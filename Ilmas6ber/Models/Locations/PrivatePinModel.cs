12a133da5d9356575e1acac57acd9109a98d6de7
opilane <opilane@HP-CZC3217062>
2026-05-20 09:04:59 +0300

    PrivatePinModel created

---
+++ Ilmas6ber/Models/Locations/PrivatePinModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ilmas6ber.Models.Locations
+{
+    public class PrivatePinModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Mapsui.MPoint Coordinates { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public DateTime? LastVisited { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
