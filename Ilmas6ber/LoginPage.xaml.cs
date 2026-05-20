e6bd22cab0063d3c802025ee783ad37b896ee972
dualify <144818434+Dualifyy@users.noreply.github.com>
2026-05-20 10:20:30 +0300

    #12 LoginPage for testing
    - Added login page to test sending data to the database
    - Very barebones with no logic right now.

--- Ilmas6ber/LoginPage.xaml.cs
+++ Ilmas6ber/LoginPage.xaml.cs
@@ -2,8 +2,25 @@ namespace Ilmas6ber;
 
 public partial class LoginPage : ContentPage
 {
+	TableView tabelview;
 	public LoginPage()
 	{
-		
+
+		new TableSection("Logi sisse")
+		{
+			new EntryCell
+			{
+				Label = "Email",
+				Placeholder = "john.doe@email.com",
+				Keyboard = Keyboard.Email,
+			},
+			new EntryCell
+			{
+				Label = "Password",
+				Placeholder = "vähemalt 6 tähte",
+				Keyboard = Keyboard.Default
+			},
+		};
+		Content = tabelview;
 	}
 }
