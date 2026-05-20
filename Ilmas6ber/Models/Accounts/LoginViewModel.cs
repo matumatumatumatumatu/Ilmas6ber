bb8f07f86cdb54fee7527d56b3de08da0153d25b
dualify <144818434+Dualifyy@users.noreply.github.com>
2026-05-20 08:53:56 +0300

    Push 2, will the folders appear now?
    .

---
+++ Ilmas6ber/Models/Accounts/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ilmas6ber.Models.Accounts
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+    }
+}
