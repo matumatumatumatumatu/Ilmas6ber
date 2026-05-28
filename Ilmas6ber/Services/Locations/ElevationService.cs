f0c6f38b8c1005c4b1cc2bd16c50c9e22d402b39
opilane <189347497+matumatumatumatumatu@users.noreply.github.com>
2026-05-28 13:36:59 +0300

    #19
    Added ElevationService for talking with the api

---
+++ Ilmas6ber/Services/Locations/ElevationService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.Json;
+
+namespace Ilmas6ber.Services.Locations
+{
+    public class ElevationService
+    {
+        private readonly HttpClient _http = new();
+
+        public async Task<double?> GetElevation(double latitude, double longitude)
+        {
+            try
+            {
+                var url = $"https://api.open-meteo.com/v1/elevation?latitude={latitude.ToString(CultureInfo.InvariantCulture)}&longitude={longitude.ToString(CultureInfo.InvariantCulture)}";
+                var response = await _http.GetStringAsync(url);
+                var doc = JsonDocument.Parse(response);
+                return doc.RootElement.GetProperty("elevation")[0].GetDouble();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
