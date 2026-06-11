3106c427501bb03ef0bbe36e42715498b37aefa0
dualify <144818434+Dualifyy@users.noreply.github.com>
2026-06-11 14:41:45 +0300

    Wire up Profile navigation from MainPage dropdown

--- Ilmas6ber/MainPage.xaml.cs
+++ Ilmas6ber/MainPage.xaml.cs
@@ -559,7 +559,7 @@ namespace Ilmas6ber
         }
         public async void DropDownProfileClicked(object sender, EventArgs e)
         {
-            //Card #25
+            await Shell.Current.GoToAsync("ProfilePage");
         }
 
         public async void DropDownPrivateClicked(object sender, EventArgs e)
